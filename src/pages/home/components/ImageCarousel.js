ba8f4a011ca9b6f1211029a9ce6639e113b9657f
jmoca <114067390+jmoca@users.noreply.github.com>
2025-04-05 23:47:18 +0200

    fix pequeños

--- src/pages/home/components/ImageCarousel.js
+++ src/pages/home/components/ImageCarousel.js
@@ -98,19 +98,17 @@ const ProgressBarCarousel = () => {
                         zIndex: currentSlide === index ? 1 : 0,
                     }}
                 >
-                    <Box sx={{ width: '100%', position: 'relative', overflow: 'hidden' }}>
-                        <Box
-                            component="img"
-                            src={slide.src}
-                            alt={slide.alt}
-                            sx={{
-                                width: '100%',
-                                height: 'auto',
-                                objectFit: 'cover',
-                            }}
-                        />
-                    </Box>
-
+                    <Box
+                        component="img"
+                        src={slide.src}
+                        alt={slide.alt}
+                        sx={{
+                            width: '100%',
+                            height: '100%',
+                            objectFit: 'cover',
+                            display: 'block'
+                        }}
+                    />
                     <Box
                         sx={{
                             position: 'absolute',
